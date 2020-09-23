abf2e9329448c14bf30e853519c9a126b7252ada
paresh Mutha <pareshmutha1990@gmail.com>
2020-09-23 10:29:08 +0530

    first commit

---
+++ precache-manifest.79d0a69110721c33b99417871f0a926a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0b7ff6325a4b86f6b893cae75ef650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c037be7b13187afe68ba",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e32d3906298d95642397",
+    "url": "/static/css/main.c0467a49.chunk.css"
+  },
+  {
+    "revision": "c037be7b13187afe68ba",
+    "url": "/static/js/2.aeb48439.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aeb48439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32d3906298d95642397",
+    "url": "/static/js/main.2ba8fe43.chunk.js"
+  },
+  {
+    "revision": "bb03b1f2debea3aff1e9",
+    "url": "/static/js/runtime-main.7bf49392.js"
+  },
+  {
+    "revision": "a1784dd2dd390bc5791d6d4729d603ac",
+    "url": "/static/media/phone_bg.a1784dd2.png"
+  },
+  {
+    "revision": "9f3de440534d20dac6eb0d21e9994a3b",
+    "url": "/static/media/play_button.9f3de440.png"
+  },
+  {
+    "revision": "79e0e6dd915c0fc0375d43a9846e1f63",
+    "url": "/static/media/shuffle_button.79e0e6dd.png"
+  }
+]);
